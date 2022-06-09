15d686a218d72ceefa6e8782129804c32cf93be8
reinaldodu <reinaldodu@gmail.com>
2022-06-09 18:49:58 -0500

    Ejercicio2 Listas JS

---
+++ Javascript/Ejercicio2_Listas/lista.js
@@ -0,0 +1,9 @@
+/* Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+- Tu nombre (string)
+- Tu edad (number)
+- ¿Eres desarrollador? (boolean)
+- Tu fecha de nacimiento (Date)
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+*/
+const lista = ["Reinaldo Duque",47,true,new Date("1974/08/13"),{titulo:"biblia",autor:"Dios", fecha:new Date(),url:"https://www.bible.com/es/"}];
+console.log(lista);
